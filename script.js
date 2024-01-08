08c908c55a7e74cd4651ab2821480911add99978
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-01-08 15:39:01 +0530

    Adding event Listener

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent=`🥳 Correct Number!!`
 
+// document.querySelector('.number').textContent=13;
+// document.querySelector('.score').textContent=20;
+
+document.querySelector('.check').addEventListener('click',function(){
+    const guess = Number(document.querySelector('.guess').value);
+    
+    if(!guess){
+        document.querySelector('.message').textContent=`⛔Aww..No Number Try Harder⛔`
+    }
+});
