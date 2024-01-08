1e489177cc1719938c717805ca223fb5a6786df5
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-01-08 17:16:53 +0530

    Bug Fixed: highscore

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 const secretNumber = Math.trunc(Math.random()*20)+1;
 console.log(secretNumber);
 let score = 10;
+let highscore = 0;
 
 document.querySelector('.again').addEventListener('click',function(){
     score = 10;
@@ -37,8 +38,10 @@ document.querySelector('.check').addEventListener('click',function(){
         document.querySelector('.number').textContent=secretNumber;
         document.querySelector('body').style.backgroundColor='#60b347';
         document.querySelector('.number').style.width = '30rem';
-
-        document.querySelector('.highscore').textContent=score;
+        if(Score>highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent=score;
+        }
     }
 
     //when guess is almost there
