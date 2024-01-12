2a57df4c328af3d475810783bf042984e52cf61f
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-01-12 21:28:09 +0530

    applying  DRY concept

--- script.js
+++ script.js
@@ -4,24 +4,28 @@ let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 10;
 let highscore = 0;
 
+function print(targetElement, message) {
+    document.querySelector(targetElement).textContent=message;
+}
 function youwin(){
-    document.querySelector('.message').textContent=`🥳 Correct Number!!`;
-    document.querySelector('.number').textContent=secretNumber;
+    print('.message',`🥳 Correct Number!!`);
+    print('.number',secretNumber);
+    document.querySelector('body').style.backgroundColor='#60b347';
     document.querySelector('body').style.backgroundColor='#60b347';
     document.querySelector('.number').style.width = '30rem';
     
-    //New HighScore
+    //New HighScore,
     if(score>highscore){
         highscore = score;
-        document.querySelector('.highscore').textContent=score;
-        document.querySelector('.message').textContent=`🎊Congratulations on achieving new HighScore🎊`;
+        print('.highscore',score);
+        print('.message',`🎊Congratulations on achieving new HighScore🎊`);
         
     }
 }
 function youlose(){
     if(score<1){
         document.querySelector('body').style.backgroundColor='#ff0000';
-        document.querySelector('.message').textContent=`💥You lost the game!`;
+        print('.message',`💥You lost the game!`);
         document.querySelector('.score').textContent=0;
         
         return 1;
@@ -31,7 +35,7 @@ function youlose(){
     }
 }
 function youlosing(show){
-    document.querySelector('.message').textContent=show;
+    print('.message',show);
     score--;
     document.querySelector('.score').textContent=youlose();
 }
@@ -40,12 +44,12 @@ document.querySelector('.check').addEventListener('click',function(){
     const guess = Number(document.querySelector('.guess').value);
     //No input
     if(!guess){
-        document.querySelector('.message').textContent=`⛔Oh..No Number Try Harder⛔`
+        print('.message',`⛔Oh..No Number Try Harder⛔`);
     }
     //if the number is not between 1 to 20
     else if(guess<1||guess>20){
-        document.querySelector('.message').textContent=`What are you Doing...
-        The number must be between 1 to 20 !!`;
+        print('.message',`What are you Doing...
+        The number must be between 1 to 20 !!`);
     }
     //Players Wins
     else if(guess === secretNumber){
@@ -66,7 +70,7 @@ document.querySelector('.check').addEventListener('click',function(){
 });
 
 document.querySelector('.number').addEventListener('click',function(){
-    document.querySelector('.message').textContent='Hey! don,t try to cheat';
+    print('.message','Hey! don,t try to cheat');
 });
 
 document.querySelector('.again').addEventListener('click',function(){
